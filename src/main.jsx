5b094621cfdcdf256eb892f63219b14589614416
Jason Garcia <jaygarcia0530@gmail.com>
2023-11-07 19:43:39 -0800

    semi colons added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { AuthProvider } from './context/AuthProvider.jsx'
-import {disableReactDevTools} from "@fvilers/disable-react-devtools"
+import './index.css';
+import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './context/AuthProvider.jsx';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
 if(process.env.NODE_ENV === 'production'){
   disableReactDevTools();
