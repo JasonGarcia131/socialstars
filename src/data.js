5f2656b374ec066a777f6953e784b03948a561ff
Jason Garcia <jaygarcia0530@gmail.com>
2023-11-11 11:08:47 -0800

    fixed typo in horoscope sagittaruis

--- src/data.js
+++ src/data.js
@@ -97,7 +97,7 @@ export const horoscopeImages = [
             light: sagitariusLight,
             shadow: sagitariusShadow
         },
-        horoscope: 'sagitarius'
+        horoscope: 'sagittarius'
     },
     {
         image: {
